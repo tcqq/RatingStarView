cf2b35d9ac38b725e363f97d8d6c45fd6d642894
tcqq <usertcqq@gmail.com>
2019-08-03 19:59:43 +0800

    清理代码

---
+++ reviewscoreview/build/docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"com.tcqq.reviewscoreview"},{"l":"所有程序包","url":"allpackages-index.html"}]
---
+++ reviewscoreview/build/docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"com.tcqq.reviewscoreview","l":"BuildConfig"},{"l":"所有类","url":"allclasses-index.html"}]
